7898526f77c5d81c7d7f9826f9e92cac7415b5b9
Xaliba <xaliba101@gmail.com>
2025-04-21 11:31:21 +0800

    feat. Add welcome and exit messages

--- AppDevCodingChallenge/Program.cs
+++ AppDevCodingChallenge/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /* quick list of what to do, not final:
+             * display welcome message
+             * check csv files exists
+             * read csv files
+             * group by devices
+             * get last 4 hours of data for each device and display:
+             * Average rainfall
+             * Green, amber or red
+             * is avergate rainfall increasing or decreasing
+             */
+
+            Console.WriteLine("Fuzion Inc");
+            Console.WriteLine("Flood detection programme");
+
+            
+            
+
+
+
+
+
+            // Completed
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
