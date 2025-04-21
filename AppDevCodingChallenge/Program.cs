3a8c1e5319fa478ac3e25d141b51d4ea4203b3a4
Xaliba <xaliba101@gmail.com>
2025-04-21 11:48:07 +0800

    feat. Error checking for device list file and readings folder and at least 1 file exists

--- AppDevCodingChallenge/Program.cs
+++ AppDevCodingChallenge/Program.cs
@@ -18,8 +18,36 @@
             Console.WriteLine("Fuzion Inc");
             Console.WriteLine("Flood detection programme");
 
-            
-            
+            // set up file and folder paths
+            string devicesFilePath = "Data/devices.csv";
+            string deviceReadingsFolderPath = "Data/Readings/";
+
+            // Error check if devices file exists
+            if (!File.Exists(devicesFilePath))
+            {
+                Console.Error.WriteLine("Devices file not found. Please confirm file exists in: " + devicesFilePath);
+                Console.ReadKey();
+                return;
+            }
+
+            // Error check if device readings folder exists
+            if (!Directory.Exists(deviceReadingsFolderPath))
+            {
+                Console.WriteLine($"Device readings folder. Please confirm folder exists in: {deviceReadingsFolderPath}");
+                Console.ReadKey();
+                return;
+            }
+
+            // Error check if theres at least 1 device readings file
+            string[] deviceReadingsFiles = Directory.GetFiles(deviceReadingsFolderPath, "*.csv");
+            if (deviceReadingsFiles.Length == 0)
+            {
+                Console.WriteLine($"No device readings files found in: {deviceReadingsFolderPath}");
+                Console.ReadKey();
+                return;
+            }
+
+
 
 
 
