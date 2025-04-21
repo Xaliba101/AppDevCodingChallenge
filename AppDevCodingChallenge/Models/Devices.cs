7f0e7b8c61661a5f1efe08f102da5e889c86672f
Xaliba <xaliba101@gmail.com>
2025-04-21 11:59:57 +0800

    feat. Added csvhelper package and created class for devices

--- AppDevCodingChallenge/Models/Devices.cs
+++ AppDevCodingChallenge/Models/Devices.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CsvHelper.Configuration.Attributes;
 
 namespace AppDevCodingChallenge.Models
 {
     internal class Devices
     {
+        [Name("Device ID")]
+        public int DeviceID { get; set; }
+
+        [Name("Device Name")]
+        public string DeviceName { get; set; }
+
+        [Name("Location")]
+        public string Location { get; set; }
     }
 }
